92c176320394af1cca4e4912261e089d9bb9b2ea
Joe Carpinelli <jdcarpinelli@gmail.com>
2020-11-18 11:52:49 -0500

    Fixing bounds error in jacobi_constant

--- src/ThreeBody/ThreeBodyCalculations.jl
+++ src/ThreeBody/ThreeBodyCalculations.jl
@@ -27,12 +27,12 @@ redimensionalize(time::T, scalar) where T<:Number = time * scalar
 """
 Returns the position w.r.t. body 1 (or 2).
 """
-position(r, xᵢ=0) = @views @. √( (r[1]-xᵢ)^2 + r[2]^2 + r[3]^2 )
+position(r, xᵢ=0) = √( (r[1]-xᵢ)^2 + r[2]^2 + r[3]^2 )
 
 """
 Returns the potential energy `U`.
 """
-potential_energy(r, μ, x₁, x₂) = @views @. (r[1]^2 + r[2]^2) + (2(1-μ)/position(r,x₁)) + (2μ/position(r,x₂))
+potential_energy(r, μ, x₁, x₂) = (r[1]^2 + r[2]^2) + (2(1-μ)/position(r,x₁)) + (2μ/position(r,x₂))
 
 """
 Returns the Jacobi Constant `C`.
