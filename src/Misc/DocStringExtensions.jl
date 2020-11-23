1f97c0c8278934952c454e5d692e7f589f502ec0
Joe Carpinelli <jdcarpinelli@gmail.com>
2020-11-23 13:36:38 -0500

    Trying out different method abbreviations

--- src/Misc/DocStringExtensions.jl
+++ src/Misc/DocStringExtensions.jl
@@ -19,16 +19,30 @@ function DocStringExtensions.format(::TerseMethods, buf, doc)
     local func = Docs.resolve(binding)
     local groups = DocStringExtensions.methodgroups(func, typesig, modname; exact = false)
     if !isempty(groups)
+
         println(buf)
         println(buf, "```julia")
         for group in groups
-            for method in group
-                DocStringExtensions.printmethod(buf, binding, func, method)
-                println(buf)
+
+            for (i, method) in enumerate(group)
+                N = length(DocStringExtensions.arguments(method))
+                # return a list of tuples that represent type signatures
+                tuples = DocStringExtensions.find_tuples(typesig)
+                # The following will find the tuple that matches the number of arguments in the function
+                # ideally we would check that the method signature matches the Tuple{...} signature
+                # but that is not straightforward because of how expressive Julia can be
+                if Sys.iswindows()
+                    t = tuples[findlast(t -> t isa DataType && t <: Tuple && length(t.types) == N, tuples)]
+                else
+                    t = tuples[findfirst(t -> t isa DataType && t <: Tuple && length(t.types) == N, tuples)]
+                end
+                DocStringExtensions.printmethod(buf, binding, func, method, t)
             end
+
         end
         println(buf, "```\n")
         println(buf)
+
     end
     return nothing
 end
