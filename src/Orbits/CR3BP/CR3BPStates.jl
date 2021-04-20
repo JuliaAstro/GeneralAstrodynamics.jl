a35ff3e232e4546ab3da5328b66cb9504553b3cf
Joe Carpinellli <jdcarpinelli@gmail.com>
2021-04-20 09:50:10 -0400

    Minimal representations now propagate

--- src/Orbits/CR3BP/CR3BPStates.jl
+++ src/Orbits/CR3BP/CR3BPStates.jl
@@ -360,7 +360,12 @@ mutable struct CircularRestrictedThreeBodyOrbit{
         F = promote_type(eltype(r), eltype(v), eltype(system), typeof(t))
         state = NormalizedCartesianState(F.(r), F.(v), F(t), frame)
         system = MinimalCircularRestrictedThreeBodySystem(F(normalized_mass_parameter(system)))
-        return new{F, typeof(NormalizedLengthUnit), typeof(NormalizedTimeUnit), typeof(state), typeof(system)}(state, system)
+        return new{F, NormalizedLengthUnit, NormalizedTimeUnit, typeof(state), typeof(system)}(state, system)
+    end
+
+    function CircularRestrictedThreeBodyOrbit(state::NormalizedCartesianState, system::MinimalCircularRestrictedThreeBodySystem, t::Real = 0; frame = Synodic)        
+        F = promote_type(typeof(state).parameters[1], typeof(system).parameters[1])
+        return new{F, NormalizedLengthUnit, NormalizedTimeUnit, typeof(state), typeof(system)}(state, system)
     end
 
     function CircularRestrictedThreeBodyOrbit(state::CartesianState, sys::CircularRestrictedThreeBodySystem)
