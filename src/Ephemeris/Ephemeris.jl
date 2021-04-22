0729984f60e7b0a6d08a31191053611d2315b075
Joe Carpinellli <jdcarpinelli@gmail.com>
2021-04-21 21:01:56 -0400

    Adding back Ephemeris module

---
+++ src/Ephemeris/Ephemeris.jl
@@ -0,0 +1,24 @@
+"""
+Provides functions to interact with Cartesian ASCII ephemeris data.
+
+This is BYOE (bring your own ephemeris) module! Check out
+JPL Horizons and 
+[wrapper scripts](https://github.com/cadojo/Halo-Explorations/blob/main/scripts/ephemeris/fetch_ephemeris.sh)
+to learn how to fetch ephemeris files.
+"""
+module Ephemeris
+
+# Module Exports
+export loadascii, Interpolator, interpolator
+
+# Module Dependencies
+using StaticArrays
+using Interpolations
+using ConcreteStructs
+using CSV, DataFrames
+using ..Orbits
+
+# Module source code
+include("LoadASCII.jl")
+
+end
--- src/UnitfulAstrodynamics.jl
+++ src/UnitfulAstrodynamics.jl
@@ -3,14 +3,16 @@ Structures and functions for handling common Astrodynamics problems! 🚀
 """
 module UnitfulAstrodynamics
 
-using Reexport
+using Reexport 
 
 include("Orbits/Orbits.jl")
 include("Propagators/Propagators.jl")
 include("OrbitPlots/OrbitPlots.jl")
+include("Ephemeris/Ephemeris.jl")
 
 @reexport using .Orbits
 @reexport using .Propagators
 @reexport using .OrbitPlots
+@reexport using .Ephemeris
 
 end # module
