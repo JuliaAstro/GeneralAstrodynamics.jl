a9203e35ac03c06303463bb6f17f95998a20a1f1
Joey Carpinelli <joey@loopy.codes>
2023-09-23 11:30:13 -0500

    Adding new analytical convenience function

--- src/CR3BPCalculations.jl
+++ src/CR3BPCalculations.jl
@@ -47,13 +47,16 @@ export distance_scaling,
     lagrange_point,
     zero_velocity_curves,
     richardson_halo,
+    richardson_ic,
     perturbation,
     convergent_direction,
     divergent_direction,
     diverge,
     diverge!,
     converge,
-    converge!
+    converge!,
+    perturb,
+    perturb!
 
 """
 The length scale factor used to nondimensionalize CR3BP states.
@@ -190,9 +193,9 @@ __Outputs:__
 __References:__
 - [Rund, 2018](https://digitalcommons.calpoly.edu/theses/1853/)
 """
-function lagrange_point(μ::Real, L = 1:5)
-    if !all(L[i] ∈ (1, 2, 3, 4, 5) for i = 1:length(L))
-        throw(ArgumentError("Requested lagrange points must be in range [1,5]"))
+function lagrange_point(μ::Real, L::Int)
+    if L < 1 || L > 5
+        error("Lagrange point index must be a value between 1 and 5 (inclusive). You provided: $L.")
     end
 
     expressions = SVector{3}(
@@ -222,15 +225,14 @@ Returns an analytical solution for a Halo orbit about `L`.
 - `steps`: Number of non-dimensional timepoints in returned state.
 
 ## Outputs
-- Synodic position vectors `r::Array{<:AbstractFloat}`
-- Synodic velocity vectors `v::Array{<:Abstractfloat}`.
-- Halo orbit period `Τ`.
+- Near-periodic initial condition `u`
+- Halo orbit period `T`.
 - Throws `ArgumentError` if L is not `1` or `2`.
 
 __References:__
 - [Rund, 2018](https://digitalcommons.calpoly.edu/theses/1853/).
 """
-function richardson_halo(μ, L::Int; Z = 0.0, hemisphere = :northern, ϕ = 0.0, steps = 1)
+function richardson_ic(μ, L::Int; Z=0.0, hemisphere=:northern, ϕ=0.0)
     if L == 1
         point = first(lagrange_point(μ, 1))
         γ = abs(one(μ) - μ - point)
@@ -241,7 +243,7 @@ function richardson_halo(μ, L::Int; Z = 0.0, hemisphere = :northern, ϕ = 0.0, 
         γ = abs(point - one(μ) + μ)
         n = collect(1:4) .* one(μ)
         c = @. ((-one(μ))^n * μ + (-one(μ))^n * (one(μ) - μ)γ^(n + 1)) /
-           (γ^3 * (one(μ) + γ^(n + 1)))
+               (γ^3 * (one(μ) + γ^(n + 1)))
     else
         throw(ArgumentError("Only Halo orbits about L1 or L2 are supported."))
     end
@@ -291,15 +293,13 @@ function richardson_halo(μ, L::Int; Z = 0.0, hemisphere = :northern, ϕ = 0.0, 
     Aₓ = √((-l₂ * Aᵧ^2 - Δ) / l₁)
 
     ν = 1 + s₁ * Aₓ^2 + s₂ * Aᵧ^2
-    Τ = 2π / (ωₚ * ν)
-    τ =
-        ν .*
-        (steps > 1 ? range(0, stop = Τ, length = steps) : range(0, stop = Τ, length = 1000))
+    T = 2π / (ωₚ * ν)
+    τ = 0
 
     if hemisphere == :northern
-        m = 1.0
+        m = 1
     elseif hemisphere == :southern
-        m = 3.0
+        m = 3
     else
         throw(ArgumentError("`hemisphere` must be `:northern` or `:southern`."))
     end
@@ -307,38 +307,179 @@ function richardson_halo(μ, L::Int; Z = 0.0, hemisphere = :northern, ϕ = 0.0, 
     δₘ = 2 - m
     τ₁ = @. ωₚ * τ + ϕ
 
-    x = @. γ * (
+    X = @. γ * (
         a₂₁ * Aₓ^2 + a₂₂ * Aᵧ^2 - Aₓ * cos(τ₁) +
         (a₂₃ * Aₓ^2 - a₂₄ * Aᵧ^2) * cos(2τ₁) +
         (a₃₁ * Aₓ^3 - a₃₂ * Aₓ * Aᵧ^2) * cos(3τ₁)
     ) + 1 - μ - (L == 1 ? γ : -γ)
-    y = @. γ * (
+    Y = @. γ * (
         k * Aₓ * sin(τ₁) +
         (b₂₁ * Aₓ^2 - b₂₂ * Aᵧ^2) * sin(2τ₁) +
         (b₃₁ * Aₓ^3 - b₃₂ * Aₓ * Aᵧ^2) * sin(3τ₁)
     )
-    z = @. γ * (
+    Z = @. γ * (
         δₘ * Aᵧ * cos(τ₁) +
         δₘ * d₂₁ * Aₓ * Aᵧ * (cos(2τ₁) - 3) +
         δₘ * (d₃₂ * Aᵧ * Aₓ^2 - d₃₁ * Aᵧ^3) * cos(3τ₁)
     )
 
-    ẋ = @. γ * (
+    Ẋ = @. γ * (
         ωₚ * ν * Aₓ * sin(τ₁) - 2ωₚ * ν * (a₂₃ * Aₓ^2 - a₂₄ * Aᵧ^2) * sin(2τ₁) -
         3ωₚ * ν * (a₃₁ * Aₓ^3 - a₃₂ * Aₓ * Aᵧ^2) * sin(3τ₁)
     )
-    ẏ = @. γ * (
+    Ẏ = @. γ * (
         ωₚ * ν * k * Aₓ * cos(τ₁) +
         2ωₚ * ν * (b₂₁ * Aₓ^2 - b₂₂ * Aᵧ^2) * cos(2τ₁) +
         3ωₚ * ν * (b₃₁ * Aₓ^3 - b₃₂ * Aₓ * Aᵧ^2) * cos(3τ₁)
     )
-    ż = @. γ * (
+    Ż = @. γ * (
         -ωₚ * ν * δₘ * Aᵧ * sin(τ₁) - 2ωₚ * ν * δₘ * d₂₁ * Aₓ * Aᵧ * sin(2τ₁) -
         3ωₚ * ν * δₘ * (d₃₂ * Aᵧ * Aₓ^2 - d₃₁ * Aᵧ^2) * sin(3τ₁)
     )
 
-    return hcat(x, y, z)[1:steps, :], hcat(ẋ, ẏ, ż)[1:steps, :], Τ
+    return (; x=X, y=Y, z=Z, ẋ=Ẋ, ẏ=Ẏ, ż=Ż), T
+
 end
+
+
+
+"""
+Returns an analytical solution for a Halo orbit about `L`.
+
+# Extended Help
+
+## Arguments 
+- `μ`: Non-dimensional mass parameter for the CR3BP system.
+- `L`: Lagrange point to orbit (L1 or L2).
+- `Z`: Desired non-dimensional Z-amplitude for Halo orbit.
+- `hemisphere`: Specifies northern or southern Halo orbit.
+- `ϕ`: Desired Halo orbit phase.
+- `steps`: Number of non-dimensional timepoints in returned state.
+
+## Outputs
+- Near-periodic initial condition `u`
+- Halo orbit period `T`.
+- Throws `ArgumentError` if L is not `1` or `2`.
+
+__References:__
+- [Rund, 2018](https://digitalcommons.calpoly.edu/theses/1853/).
+"""
+function richardson_halo(μ, L::Int; Z=0.0, hemisphere=:northern, ϕ=0.0, length=10)
+    if L == 1
+        point = first(lagrange_point(μ, 1))
+        γ = abs(one(μ) - μ - point)
+        n = collect(1:4) .* one(μ)
+        c = @. (μ + (-one(1))^n * (one(μ) - μ)γ^(n + 1)) / (γ^3 * (one(μ) - γ^(n + 1)))
+    elseif L == 2
+        point = first(lagrange_point(μ, 2))
+        γ = abs(point - one(μ) + μ)
+        n = collect(1:4) .* one(μ)
+        c = @. ((-one(μ))^n * μ + (-one(μ))^n * (one(μ) - μ)γ^(n + 1)) /
+               (γ^3 * (one(μ) + γ^(n + 1)))
+    else
+        throw(ArgumentError("Only Halo orbits about L1 or L2 are supported."))
+    end
+
+    if length < 2
+        throw(ArgumentError("The trajectory lenth must be two or greater. You provided: $length."))
+    end
+
+    ωₚ = √((2 - c[2] + √((9c[2]^2 - 8c[2]))) / 2)
+    k = (ωₚ^2 + 1 + 2c[2]) / (2ωₚ)
+
+    d₁ = (3ωₚ^2 / k) * (k * (6ωₚ^2 - 1) - 2ωₚ)
+    d₂ = (8ωₚ^2 / k) * (k * (11ωₚ^2 - 1) - 2ωₚ)
+    a₂₁ = (3c[3] * (k^2 - 2)) / (4(1 + 2c[2]))
+    a₂₂ = (3c[3]) / (4(1 + 2c[2]))
+    a₂₃ = (-3c[3]ωₚ / (4k * d₁)) * (3k^3 * ωₚ - 6k * (k - ωₚ) + 4)
+    a₂₄ = (-3c[3]ωₚ / (4k * d₁)) * (2 + 3k * ωₚ)
+    b₂₁ = (-3c[3]ωₚ / (2d₁)) * (3k * ωₚ - 4)
+    b₂₂ = -3c[3] * ωₚ / d₁
+    d₂₁ = -c[3] / (2ωₚ^2)
+    a₃₁ =
+        (-9ωₚ / (4d₂)) * (4c[3] * (k * a₂₃ - b₂₁) + k * c[4] * (4 + k^2)) +
+        ((9ωₚ^2 + 1 - c[2]) / (2d₂)) * (3c[3] * (2a₂₃ - k * b₂₁) + c[4] * (2 + 3k^2))
+    a₃₂ =
+        (-9ωₚ / (4d₂)) * (4c[3] * (3k * a₂₄ - b₂₂) + k * c[4]) -
+        (3 / (2d₂)) * (9ωₚ^2 + 1 - c[2]) * (c[3] * (k * b₂₂ + d₂₁ - 2a₂₄) - c[4])
+    b₃₁ =
+        (3 / (8d₂)) * 8ωₚ * (3c[3] * (k * b₂₁ - 2a₂₃) - c[4] * (2 + 3k^2)) +
+        (3 / (8d₂)) * (9ωₚ^2 + 1 + 2c[2]) * (4c[3] * (k * a₂₃ - b₂₁) + k * c[4] * (4 + k^2))
+    b₃₂ =
+        (9ωₚ / d₂) * (c[3] * (k * b₂₂ + d₂₁ - 2a₂₄) - c[4]) +
+        (3(9ωₚ^2 + 1 + 2c[2]) / (8d₂) * (4c[3] * (k * a₂₄ - b₂₂) + k * c[4]))
+    d₃₁ = (3 / (64ωₚ^2)) * (4c[3] * a₂₄ + c[4])
+    d₃₂ = (3 / (64 + ωₚ^2)) * (4c[3] * (a₂₃ - d₂₁) + c[4] * (4 + k^2))
+
+    s₁ =
+        (1 / (2ωₚ * (ωₚ * (1 + k^2) - 2k))) * (
+            3c[3] / 2 * (2a₂₁ * (k^2 - 2) - a₂₃ * (k^2 + 2) - 2k * b₂₁) -
+            (3c[4] / 8) * (3k^4 - 8k^2 + 8)
+        )
+    s₂ =
+        (1 / (2ωₚ * (ωₚ * (1 + k^2) - 2k))) * (
+            3c[3] / 2 * (2a₂₂ * (k^2 - 2) + a₂₄ * (k^2 + 2) + 2k * b₂₂ + 5d₂₁) +
+            (3c[4] / 8) * (12 - k^2)
+        )
+    l₁ = (-3c[3] / 2) * (2a₂₁ + a₂₃ + 5d₂₁) - (3c[4] / 8) * (12 - k^2) + 2ωₚ^2 * s₁
+    l₂ = (3c[3] / 2) * (a₂₄ - 2a₂₂) + (9c[4] / 8) + 2ωₚ^2 * s₂
+    Δ = ωₚ^2 - c[2]
+
+    Aᵧ = Z / γ
+    Aₓ = √((-l₂ * Aᵧ^2 - Δ) / l₁)
+
+    ν = 1 + s₁ * Aₓ^2 + s₂ * Aᵧ^2
+    T = 2π / (ωₚ * ν)
+    τ = ν .* range(0, stop=T, length=length)
+
+    if hemisphere == :northern
+        m = 1
+    elseif hemisphere == :southern
+        m = 3
+    else
+        throw(ArgumentError("`hemisphere` must be `:northern` or `:southern`."))
+    end
+
+    δₘ = 2 - m
+    τ₁ = @. ωₚ * τ + ϕ
+
+    X = @. γ * (
+        a₂₁ * Aₓ^2 + a₂₂ * Aᵧ^2 - Aₓ * cos(τ₁) +
+        (a₂₃ * Aₓ^2 - a₂₄ * Aᵧ^2) * cos(2τ₁) +
+        (a₃₁ * Aₓ^3 - a₃₂ * Aₓ * Aᵧ^2) * cos(3τ₁)
+    ) + 1 - μ - (L == 1 ? γ : -γ)
+    Y = @. γ * (
+        k * Aₓ * sin(τ₁) +
+        (b₂₁ * Aₓ^2 - b₂₂ * Aᵧ^2) * sin(2τ₁) +
+        (b₃₁ * Aₓ^3 - b₃₂ * Aₓ * Aᵧ^2) * sin(3τ₁)
+    )
+    Z = @. γ * (
+        δₘ * Aᵧ * cos(τ₁) +
+        δₘ * d₂₁ * Aₓ * Aᵧ * (cos(2τ₁) - 3) +
+        δₘ * (d₃₂ * Aᵧ * Aₓ^2 - d₃₁ * Aᵧ^3) * cos(3τ₁)
+    )
+
+    Ẋ = @. γ * (
+        ωₚ * ν * Aₓ * sin(τ₁) - 2ωₚ * ν * (a₂₃ * Aₓ^2 - a₂₄ * Aᵧ^2) * sin(2τ₁) -
+        3ωₚ * ν * (a₃₁ * Aₓ^3 - a₃₂ * Aₓ * Aᵧ^2) * sin(3τ₁)
+    )
+    Ẏ = @. γ * (
+        ωₚ * ν * k * Aₓ * cos(τ₁) +
+        2ωₚ * ν * (b₂₁ * Aₓ^2 - b₂₂ * Aᵧ^2) * cos(2τ₁) +
+        3ωₚ * ν * (b₃₁ * Aₓ^3 - b₃₂ * Aₓ * Aᵧ^2) * cos(3τ₁)
+    )
+    Ż = @. γ * (
+        -ωₚ * ν * δₘ * Aᵧ * sin(τ₁) - 2ωₚ * ν * δₘ * d₂₁ * Aₓ * Aᵧ * sin(2τ₁) -
+        3ωₚ * ν * δₘ * (d₃₂ * Aᵧ * Aₓ^2 - d₃₁ * Aᵧ^2) * sin(3τ₁)
+    )
+
+    return [
+        (; x=x, y=y, z=z, ẋ=ẋ, ẏ=ẏ, ż=ż)
+        for (x, y, z, ẋ, ẏ, ż) in zip(X, Y, Z, Ẋ, Ẏ, Ż)
+    ], T
+
+end
+
 
 """
 Returns a `Vector` of `Matrix` values.
@@ -350,7 +491,7 @@ function zero_velocity_curves(
     r::AbstractVector,
     v::AbstractVector,
     μ::Real;
-    nondimensional_range = range(-2; stop = 2, length = 1000),
+    nondimensional_range=range(-2; stop=2, length=1000)
 )
     x = nondimensional_range
     y = nondimensional_range
@@ -433,19 +574,33 @@ function divergent_direction(stm::AbstractMatrix)
 end
 
 """
-Perturbs a Cartesian state along a halo orbit onto the provided direction of the provided manifold.
+Return the perturbation in Cartesian state space along a halo orbit onto the provided direction of the provided manifold.
 """
-function perturbation(stm::AbstractMatrix, direction::AbstractVector; eps = 1e-8)
+function perturbation(stm::AbstractMatrix, direction::AbstractVector; eps=1e-8)
     return eps * normalize(stm * normalize(direction))
 end
 
+"""
+Perturb a Cartesian state along a halo orbit onto a stable or unstable manifold.
+"""
+function perturb(u::AbstractVector, stm::AbstractMatrix, direction::AbstractVector; eps=1e-8)
+    p = similar(u)
+    perturb!(p, u, stm, direction; eps=eps)
+end
+
+"""
+Perturb a Cartesian state in-place along a halo orbit onto a stable or unstable manifold.
+"""
+function perturb!(p::AbstractVector, u::AbstractVector, stm::AbstractMatrix, direction::AbstractVector; eps=1e-8)
+    p .= @views(u[begin:begin+5]) + perturbation(stm, direction; eps=eps)
+end
 
 """
 Perturb halo orbit conditions onto the orbit's unstable manifold.
 """
-function diverge(u::AbstractVector, stm::AbstractMatrix, Φ::AbstractMatrix; eps = 1e-8)
+function diverge(u::AbstractVector, stm::AbstractMatrix, Φ::AbstractMatrix; eps=1e-8)
     p = similar(u)
-    return diverge!(p, u, stm, Φ; eps = eps)
+    return diverge!(p, u, stm, Φ; eps=eps)
 end
 
 """
@@ -456,15 +611,15 @@ diverge!(
     u::AbstractVector,
     stm::AbstractMatrix,
     Φ::AbstractMatrix;
-    eps = 1e-8,
-) = (p .= @views(u[begin:begin+5]) + perturbation(stm, divergent_direction(Φ); eps = eps))
+    eps=1e-8
+) = (p .= @views(u[begin:begin+5]) + perturbation(stm, divergent_direction(Φ); eps=eps))
 
 """
 Perturb halo orbit conditions onto the orbit's unstable manifold.
 """
-function converge(u::AbstractVector, stm::AbstractMatrix, Φ::AbstractMatrix; eps = 1e-8)
+function converge(u::AbstractVector, stm::AbstractMatrix, Φ::AbstractMatrix; eps=1e-8)
     p = similar(u)
-    return converge!(p, u, stm, Φ; eps = eps)
+    return converge!(p, u, stm, Φ; eps=eps)
 end
 
 """
@@ -475,8 +630,7 @@ converge!(
     u::AbstractVector,
     stm::AbstractMatrix,
     Φ::AbstractMatrix;
-    eps = 1e-8,
-) = (p .= @views(u[begin:begin+5]) + perturbation(stm, convergent_direction(Φ); eps = eps))
-
+    eps=1e-8
+) = (p .= @views(u[begin:begin+5]) + perturbation(stm, convergent_direction(Φ); eps=eps))
 
 end
