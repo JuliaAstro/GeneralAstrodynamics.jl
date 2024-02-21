a4caeb9c77d8dd67843951800d9ef3efffab48c3
Joey Carpinelli <joey@loopy.codes>
2024-02-20 22:43:56 -0500

    Updates [compat]; removes test

--- src/GeneralAstrodynamics.jl
+++ src/GeneralAstrodynamics.jl
@@ -30,9 +30,33 @@ using DocStringExtensions
                                $(DOCSTRING)
                                """
 
+"""
+Load several small SPICE kernels to support simple operations. The kernels
+loaded into memory sum to less than 11MB.
+
+# Extended Help
+
+Calls `SPICE.furnsh` on the following kernels.
+
+- `de432s.bsp` (approximately 10MB)
+- `latest_leapseconds_lsk.tls` (approximately 5KB)
+- `gm_de440.tpc` (approximately 12KB)
+- `pck00011.tpc` (approximately 130KB)
+"""
+function load_generic_spice_kernels!()
+    return furnsh(
+        de432s(),                   # position and velocity data for nearby planets
+        latest_leapseconds_lsk(),   # timekeeping, parsing epochs
+        gm_de440(),                 # mass parameters for major solar system bodies
+        pck00011(),                 # physical properties of major solar system bodies
+    )
+end
+
 @reexport using AstrodynamicalCalculations
 @reexport using AstrodynamicalModels
 @reexport using AstrodynamicalSolvers
-@reexport using SPICE, SPICEKernels, SPICEBodies
+@reexport using SPICE: furnsh
+@reexport using SPICEKernels
+@reexport using SPICEBodies
 
 end # module
