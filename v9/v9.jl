6a98e3809fc9daea2047e4da595b7f4d807ba037
Joey Carpinelli <joey@loopy.codes>
2024-02-23 23:39:48 -0500

    Adding MTK v8/v9 experiment

---
+++ v9/v9.jl
@@ -0,0 +1,19 @@
+using Pkg
+Pkg.activate(@__DIR__)
+
+using ModelingToolkit
+using AstrodynamicalModels
+
+x = collect(0.1:0.1:0.6)
+p = collect(0.1:0.1:0.7)
+t = NaN
+
+model = complete(PlanarEntrySystem())
+field = ODEFunction(model)
+
+@show field(x, p, t)
+
+open(joinpath(@__DIR__, "entry.jl"), "w") do file
+    func = string(generate_function(model)[1])
+    write(file, func)
+end
