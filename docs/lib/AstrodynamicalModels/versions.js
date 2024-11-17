4a74461a242468a92dc98dcc168dd4fd86a24f3f
Documenter.jl <documenter@juliadocs.github.io>
2024-11-17 22:19:17 +0000

    build based on c73592d

--- docs/lib/AstrodynamicalModels/versions.js
+++ docs/lib/AstrodynamicalModels/versions.js
@@ -1,5 +1,9 @@
 var DOC_VERSIONS = [
+  "stable",
+  "v3.0",
+  "v2.0",
+  "v1.3",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "dev";
-var DOCUMENTER_STABLE = "dev";
+var DOCUMENTER_NEWEST = "v3.0.0";
+var DOCUMENTER_STABLE = "stable";
